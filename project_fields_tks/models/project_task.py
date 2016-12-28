0dd0cfa3248204c177d1a39fdecf6cedfd6463d3
Yoshi Tashiro <tashiro@roomsfor.hk>
2016-12-28 22:14:08 +0800

    [IMP] logic to update budget_amt

--- project_fields_tks/models/project_task.py
+++ project_fields_tks/models/project_task.py
@@ -14,8 +14,9 @@ class ProjectTask(models.Model):
         default=0.0
     )
     budget_amt = fields.Monetary(
+        compute='_update_budget_amt',
+        store=True,
         string='Budget Amount',
-        # default=0.0,
     )
     currency_id = fields.Many2one(
         related='project_id.currency_id',
@@ -23,7 +24,15 @@ class ProjectTask(models.Model):
         string='Currency',
         readonly=True)
 
-    # @api.onchange('budget_ratio', 'project_id.budget_amt')
-    # def onchange_partner(self):
-    #     for invoice in self:
-    #         invoice.delivery_note = invoice.partner_id.delivery_note
+
+    @api.onchange('budget_ratio')
+    def onchange_budget_ratio(self):
+        for task in self:
+            task.budget_amt = task.project_id.budget_amt * task.budget_ratio \
+                              / 100
+
+    @api.depends('project_id.budget_amt')
+    def _update_budget_amt(self):
+        for task in self:
+            task.budget_amt = task.project_id.budget_amt * task.budget_ratio \
+                              / 100
