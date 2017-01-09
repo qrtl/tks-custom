e959d790ca966cc8234a9df7251e92146e10ef8c
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-01-09 23:16:25 +0800

    [IMP] add logic to unlink analytic line

--- project_task_analytic/models/project_task_type.py
+++ project_task_analytic/models/project_task_type.py
@@ -8,6 +8,15 @@ from odoo import api, models, fields
 class ProjectTaskType(models.Model):
     _inherit = 'project.task.type'
 
-    done_stage = fields.Boolean(
-        'Done Stage'
+    # unfinished = fields.Boolean(
+    #     'Unfinished Stage'
+    # )
+    # finished = fields.Boolean(
+    #     'Done Stage'
+    # )
+    stage_state = fields.Selection(
+        [('to_do', 'To Do'),
+         ('done', 'Done'),
+         ('no_need', 'Not Needed')],
+        'Stage State',
     )
