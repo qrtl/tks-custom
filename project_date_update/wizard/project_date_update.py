f32fcd9150acdc340ae596d7e444caac4e53427c
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-02-28 22:32:11 +0800

    [IMP] add logic to update task deadlines

--- project_date_update/wizard/project_date_update.py
+++ project_date_update/wizard/project_date_update.py
@@ -2,6 +2,7 @@
 # Copyright 2017 Rooms For (Hong Kong) Limited T/A OSCG
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+import datetime
 from odoo import api, fields, models, _
 
 
@@ -32,5 +33,25 @@ class ProjectDateUpate(models.TransientModel):
 
 
     @api.multi
+    def update_task_dates(self, project):
+        self.ensure_one()
+        for task in project.task_ids:
+            task.date_deadline = fields.Date.to_string(
+                fields.Date.from_string(project.date) -
+                datetime.timedelta(days=task.days_project_due)
+            )
+        return
+
+    @api.multi
     def update_project_dates(self):
+        context = dict(self._context or {})
+        active_ids = context.get('active_ids', [])
+        active_model = context.get('active_model', False)
+        project = self.env[active_model].browse(active_ids)
+        default = {
+            'date_start': self.start_date,
+            'date': self.end_date,
+        }
+        project.write(default)
+        self.update_task_dates(project)
         return
