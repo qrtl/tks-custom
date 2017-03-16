9caa519c189cbb8e48f3893ef127ffa7d08ab778
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-03-17 00:00:11 +0800

    [IMP] handle material subtotal requirement

---
+++ report_sale_tks/models/sale_order_line.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Rooms For (Hong Kong) Limited T/A OSCG
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+    _order = 'order_id, layout_category_id, matl_subtotal desc, sequence, id'
+
+    matl_subtotal = fields.Boolean(
+        'Material Subtotal',
+    )
