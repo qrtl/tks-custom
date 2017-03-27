cb38311f798ea330b31d4829369d8fc47a8d264a
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-03-27 16:15:01 +0800

    [IMP] default matl_subtotal from product

--- report_sale_tks/models/__init__.py
+++ report_sale_tks/models/__init__.py
@@ -2,6 +2,7 @@
 # Copyright 2017 Rooms For (Hong Kong) Limited T/A OSCG
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from . import product_template
 from . import sale_layout_category_desc
 from . import sale_order
 from . import sale_order_line
---
+++ report_sale_tks/models/product_template.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Rooms For (Hong Kong) Limited T/A OSCG
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    matl_subtotal = fields.Boolean(
+        'Material Subtotal',
+    )
