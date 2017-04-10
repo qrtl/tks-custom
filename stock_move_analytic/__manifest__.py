45b4c8d7b8176df7be6b1b74dcb7ef0435732aa5
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-04-10 22:58:22 +0800

    [IMP] misc styling adjustments

--- stock_move_analytic/__manifest__.py
+++ stock_move_analytic/__manifest__.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 Rooms For (Hong Kong) Limited T/A OSCG
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2017 Rooms For (Hong Kong) Limited T/A OSCG
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
-    'name': "Delivary Stock Move Analytic",
+    'name': "Delivery Stock Move Analytic",
     'version': "10.0.1.0.0",
     'author': "Rooms For (Hong Kong) Limited T/A OSCG",
     'website': "https://www.odoo-asia.com/",
     'category': "Warehouse",
     'license': "AGPL-3",
-    'depends': ['sale', 'stock', 'sale_line_analytic'],
+    'depends': [
+        'sale',
+        'stock',
+        'sale_line_analytic'
+    ],
     'data': [
     ],
     "application": False,
