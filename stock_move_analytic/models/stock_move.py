dd628a849749b702017ed03958050d475c47af42
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-04-13 18:51:16 +0800

    [IMP] add conditions to related_project_id field

--- stock_move_analytic/models/stock_move.py
+++ stock_move_analytic/models/stock_move.py
@@ -11,4 +11,20 @@ class StockMove(models.Model):
     related_project_id = fields.Many2one(
         'account.analytic.account',
         string='Analytic Account',
-        help="Analytic account related to stock move.")
+        help="Analytic account related to stock move."
+    )
+    need_analytic = fields.Boolean(
+        string='Need Analytic Account',
+        compute='_compute_need_analytic',
+        store=True,
+    )
+
+    @api.multi
+    @api.depends('location_id', 'location_dest_id')
+    def _compute_need_analytic(self):
+        for move in self:
+            if move.location_id.usage == 'customer' or \
+                            move.location_dest_id.usage == 'customer':
+                move.need_analytic = True
+            else:
+                move.need_analytic = False
