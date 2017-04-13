4b95025a7d3843d3abd3b058c8a4c9e522476dd1
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-04-13 23:02:09 +0800

    [IMP] adjust pivot view

--- account_analytic_type/models/analytic_type.py
+++ account_analytic_type/models/analytic_type.py
@@ -7,18 +7,23 @@ from openerp import fields, models
 
 class AnalyticType(models.Model):
     _name = 'analytic.type'
+    _order = 'sequence'
 
     name = fields.Char(
         string="Name",
         required=True
     )
     analytic_type = fields.Selection(selection=[
+        ('sales', 'Sales'),
         ('ev_actual', 'EV (Actual)'),
         ('labour', 'Labour'),
-        ('sales', 'Sales'),
-        ('overhead', 'Overhead'),
         ('material_costs', 'Material Costs'),
+        ('overhead', 'Overhead'),
         ('outsourcing', 'Outsourcing')],
         string='Analytic Type',
+        required=True,
+    )
+    sequence = fields.Integer(
+        default=10,
         required=True,
     )
