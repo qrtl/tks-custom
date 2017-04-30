42645e0c56001a7d770a316d2a18ca9d1edc35c7
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-04-30 13:21:53 +0800

    [ADD] report_account_tks

---
+++ report_account_tks/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Rooms For (Hong Kong) Limited T/A OSCG
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Invoice Print",
+    "summary": "",
+    "version": "10.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://www.odoo-asia.com/",
+    "author": "Rooms For (Hong Kong) Limited T/A OSCG",
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "pre_init_hook": "",
+    "post_init_hook": "",
+    "post_load": "",
+    "uninstall_hook": "",
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "account",
+        "report_common_tks",
+    ],
+    "data": [
+        'report/account_report_invoice.xml',
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ]
+}
