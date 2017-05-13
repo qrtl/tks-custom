c4b47166a0c101388f54ae9036ed3e49ed3234c3
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-05-13 14:22:42 +0800

    Activity profit (#86)
    * [IMP] update model to calculate activity profit
    
    * [IMP] circumvent access error when time is updated by project user

--- project_activity/__manifest__.py
+++ project_activity/__manifest__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Rooms For (Hong Kong) Limited T/A OSCG
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2017 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Project Activities",
     "summary": "",
-    "version": "10.0.1.2.0",
+    "version": "10.0.1.3.0",
     "category": "Project",
     "website": "https://www.odoo-asia.com/",
-    "author": "Rooms For (Hong Kong) Limited T/A OSCG",
+    "author": "Quartile Limited",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
@@ -22,6 +22,7 @@
     "depends": [
         "project_fields_tks",
         "project_task_analytic",
+        "sale_timesheet",
     ],
     "data": [
         'security/ir.model.access.csv',
