a501fda8824a1d2b845abd84e3c64f4d5a45eeea
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-05-26 15:29:59 +0800

    [ADD] report_purchase_tks

---
+++ report_purchase_tks/models/purchase_order.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    doc_title = fields.Char(
+        string="Doc Title",
+    )
